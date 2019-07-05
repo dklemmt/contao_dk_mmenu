183ef37c45fd90563c267fe5ea8cd7c8e656619f
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2019-07-05 15:45:10 +0200

    minor change to translation

--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -23,5 +23,5 @@ $GLOBALS['TL_LANG']['FMD']['mmenu'][0] = 'mmenu - navigation menu';
 $GLOBALS['TL_LANG']['FMD']['mmenu'][1] = 'Generates an alternative navigation menu from the site structure - not only - for mobile Sites.';
 $GLOBALS['TL_LANG']['FMD']['mmenuCustom'][0] = 'mmenu - custom navigation';
 $GLOBALS['TL_LANG']['FMD']['mmenuCustom'][1] = 'Generates an alternative custom navigation - not only - for mobile Sites.';
-$GLOBALS['TL_LANG']['FMD']['mmenuHtml'][0] = 'mmenu - Html';
-$GLOBALS['TL_LANG']['FMD']['mmenuHtml'][1] = 'Inserts custom html into mmenu.';
+$GLOBALS['TL_LANG']['FMD']['mmenuHtml'][0] = 'mmenu - HTML';
+$GLOBALS['TL_LANG']['FMD']['mmenuHtml'][1] = 'Inserts custom HTML into mmenu.';
